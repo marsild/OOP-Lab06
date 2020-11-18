199bd71d43bfd1f8b63e1491f6e30cccd06b8bd2
Marsild <marsild.spahiu@studio.unibo.it>
2020-11-18 18:00:25 +0100

    fine esercizio 3 [SOL]

--- src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/collections2/SocialNetworkUserImpl.java
@@ -1,7 +1,6 @@
 package it.unibo.oop.lab.collections2;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * 
@@ -13,70 +12,85 @@ import java.util.List;
  * 1) complete the definition of the methods by following the suggestions
  * included in the comments below.
  * 
- * @param <U>
- *            Specific user type
+ * @param <U> Specific user type
  */
 public class SocialNetworkUserImpl<U extends User> extends UserImpl implements SocialNetworkUser<U> {
 
-    /*
-     * 
-     * [FIELDS]
-     * 
-     * Define any necessary field
-     * 
-     * In order to save the people followed by a user organized in groups, adopt
-     * a generic-type Map:
-     * 
-     * think of what type of keys and values would best suit the requirements
-     */
+	/*
+	 * 
+	 * [FIELDS]
+	 * 
+	 * Define any necessary field
+	 * 
+	 * In order to save the people followed by a user organized in groups, adopt a
+	 * generic-type Map:
+	 * 
+	 * think of what type of keys and values would best suit the requirements
+	 */
+	Map<String, Set<U>> people;
+	/*
+	 * [CONSTRUCTORS]
+	 * 
+	 * 1) Complete the definition of the constructor below, for building a user
+	 * participating in a social network, with 4 parameters, initializing:
+	 * 
+	 * - firstName - lastName - username - age and every other necessary field
+	 * 
+	 * 2) Define a further constructor where age is defaulted to -1
+	 */
 
-    /*
-     * [CONSTRUCTORS]
-     * 
-     * 1) Complete the definition of the constructor below, for building a user
-     * participating in a social network, with 4 parameters, initializing:
-     * 
-     * - firstName - lastName - username - age and every other necessary field
-     * 
-     * 2) Define a further constructor where age is defaulted to -1
-     */
+	/**
+	 * Builds a new {@link SocialNetworkUserImpl}.
+	 * 
+	 * @param name    the user firstname
+	 * @param surname the user lastname
+	 * @param userAge user's age
+	 * @param user    alias of the user, i.e. the way a user is identified on an
+	 *                application
+	 */
+	public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
+		super(name, surname, user, userAge);
+		this.people = new HashMap<>();
+	}
 
-    /**
-     * Builds a new {@link SocialNetworkUserImpl}.
-     * 
-     * @param name
-     *            the user firstname
-     * @param surname
-     *            the user lastname
-     * @param userAge
-     *            user's age
-     * @param user
-     *            alias of the user, i.e. the way a user is identified on an
-     *            application
-     */
-    public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(name, surname, user, userAge);
-    }
+	public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+		this(name, surname, user, -1);
+	}
 
-    /*
-     * [METHODS]
-     * 
-     * Implements the methods below
-     */
+	/*
+	 * [METHODS]
+	 * 
+	 * Implements the methods below
+	 */
 
-    @Override
-    public boolean addFollowedUser(final String circle, final U user) {
-        return false;
-    }
+	@Override
+	public boolean addFollowedUser(final String circle, final U user) {
+		Set<U> persone = this.people.get(circle);
+		if(persone == null) {
+			persone = new HashSet<U>();
+			this.people.put(circle, persone);
+		}
+		
+		return this.people.get(circle).add(user);
+		
+	}
 
-    @Override
-    public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
-    }
+	@Override
+	public Collection<U> getFollowedUsersInGroup(final String groupName) {
+		Collection<U> returnList = this.people.get(groupName);
+		if(returnList==null) {
+			return Collections.emptyList();
+		}
+		return new ArrayList<>(returnList);
+	}
 
-    @Override
-    public List<U> getFollowedUsers() {
-        return null;
-    }
+	@Override
+	public List<U> getFollowedUsers() {
+		final Set<U> followedUsers = new HashSet<>();
+		for(final Set<U> group : this.people.values()) {
+			followedUsers.addAll(group);
+		}
+		return new ArrayList<U>(followedUsers);
+	}
 
 }
